7f6f33b5610ef62dc1100ecf064d65b8116f9b67
ahmed hawary <hawary.1311@gmail.com>
2022-12-14 02:27:41 +0200

    add project material

--- Data Modeling With Postgres - Projejct/project-template/etl.py
+++ Data Modeling With Postgres - Projejct/project-template/etl.py
@@ -6,66 +6,95 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """ingesting and transformong data from JSPON to Pandas DF
+
+    Args:
+        cur: cusor of psycopg2.
+        filepath: json log file path.
+    """
+
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
-    
-    # insert artist record
-    artist_data = 
+
+    #     # insert artist record
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[
+        0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    """Ingesting and transforming songs data from JSOS format into Pandas DFs
+
+    Args:
+        cur: cusor of psycopg2.
+        filepath: json log file path
+    """
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
-    
+    dt = pd.to_datetime(df['ts'], unit='ms')
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [dt, dt.dt.hour, dt.dt.day, dt.dt.weekofyear, dt.dt.month, dt.dt.year, dt.dt.weekday]
+    column_labels = ('timestamp', 'hour', 'day', 'week of year', 'month', 'year', 'weekday')
+    data_dict = dict(zip(column_labels, time_data))
+    time_df = pd.DataFrame.from_dict(data_dict)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
+    df['ts'] = pd.to_datetime(df['ts'], unit='ms')
+
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """Processes JSON files for a data directory path.
+
+    Valid function values can be 'process_song_file' or
+    'process_log_file'.
+
+    Args:
+        cur: cusor of psycopg2
+        conn: A database connection of psycopg2
+        filepath: json data file path
+        func: The function to invoke
+    """
+
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
